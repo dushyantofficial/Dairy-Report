069214101cf2e7dbc598eb617eaa9106f36544a2
mtech <dushyantchhatraliya@gmail.com>
2023-02-19 18:43:23 +0530

    Code Implement Change

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -2,6 +2,7 @@
     @php
         $year = \Carbon\Carbon::now()->format('Y')
     @endphp
+
     <div class="copyright">
         {{$year}} &copy; Copyright <strong><span>DairyReport</span></strong>. All Rights Reserved
     </div>
