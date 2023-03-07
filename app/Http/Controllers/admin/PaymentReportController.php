fe8567b694c7c843e238173ba23972111ad45cab
gaurav253 <snehalworks123@gmail.com>
2023-03-07 14:51:13 +0530

    code Update

--- app/Http/Controllers/admin/PaymentReportController.php
+++ app/Http/Controllers/admin/PaymentReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\admin\Customers;
+use App\Models\admin\ItemSales;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -47,17 +48,26 @@ class PaymentReportController extends Controller
     public function payment_deduct_report(Request $request){
         $user = Auth::user();
         if ($user->role == config('constants.ROLE.ADMIN')) {
-            $filter_customers = Customers::orderBy('id','desc')->get();
+            $filter_payment_deduct = ItemSales::orderBy('id', 'desc')->get();
+            $bank_names = Customers::orderBy('bank_name', 'asc')->distinct()->get();
             if ($request->date) {
                 $date = $request->date;
                 $name = explode(' ', $date);
                 $start = date('Y-m-d', strtotime($name[0]));
                 $end = date('Y-m-d', strtotime($name[2]));
-                $filter_customers = Customers::whereDate('created_at', '>=', $start)->whereDate('created_at', '<=', $end)->get();
+
+                $filter_payment_deduct = Customers::where(function ($query) use ($request,$start,$end){
+                    $query->whereBetween('customer_code',[$request->customer_from_code,$request->customer_to_code]);
+                    $query->orWhere('bank_name','==',$request->bank_name);
+                    $query->whereDate('created_at', '>=', $start);
+                    $query->whereDate('created_at', '>=', $end);
+                })->get();
+
             }
-            return view('admin.report.customer.customer_report_show', compact('filter_customers'));
+            return view('admin.payment.payment_deduct_report', compact('filter_payment_deduct', 'bank_names'));
         }
-        $filter_customers = Customers::where('user_id', $user->id)->orderBy('id','desc')->get();
+        $filter_payment_deduct = ItemSales::where('created_by', $user->id)->orderBy('id', 'desc')->get();
+        $bank_names = Customers::where('user_id', $user->id)->orderBy('bank_name', 'desc')->get();
         if ($request->date) {
             $date = $request->date;
             $name = explode(' ', $date);
@@ -65,7 +75,7 @@ class PaymentReportController extends Controller
             $end = date('Y-m-d', strtotime($name[2]));
             $filter_customers = Customers::where('user_id', $user->id)->whereDate('created_at', '>=', $start)->whereDate('created_at', '<=', $end)->get();
         }
-        return view('admin.report.customer.customer_report_show', compact('filter_customers'));
+        return view('admin.payment.payment_deduct_report', compact('filter_payment_deduct', 'bank_names'));
     }
 
 
